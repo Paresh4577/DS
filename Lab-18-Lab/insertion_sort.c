97b88777a12c3be60631f5b6c91432f6d8fa9c71
Paresh4577 <142432711+Paresh4577@users.noreply.github.com>
2024-03-17 23:06:53 +0530

    Add files via upload

---
+++ Lab-18-Lab/insertion_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+void insertion(int a[],int len){
+	int key,j,i;
+	
+	for(i=1;i<len;i++){
+		key = a[i];
+		j = i-1;
+		
+		while(j >=0 && a[j] > key){
+			 a[j+1] = a[j];
+			 j = j-1;
+		}
+		a[j+1] = key;
+		
+	}
+}
+
+void main(){
+	int len,i;
+	
+    int	a[] = {78,56,33,23,2,8,99};
+	len = sizeof(a)/sizeof(a[0]);
+	insertion(a,len);
+	printf("After Sorting Elements\n");
+	for(i=0;i<len;i++){
+			printf("a[%d] = %d\n",i, a[i]);
+	}
+	
+}
